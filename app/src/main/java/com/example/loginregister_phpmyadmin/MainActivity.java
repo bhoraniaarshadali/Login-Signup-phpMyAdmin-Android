1a7d3d7cc96ea90536b96fb3b3d00ce1c89eead3
Arshadali <bhoraniyaarshadali431@gmail.com>
2024-06-01 16:52:49 +0530

    Login & Signup .xml

---
+++ app/src/main/java/com/example/loginregister_phpmyadmin/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.loginregister_phpmyadmin;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView test1 = (TextView) findViewById(R.id.test);
+
+        test1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
